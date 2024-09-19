293c707fe45a0e2674c08614c1e4c8f37dbc4f15
Michael Plank <michael.plank@canterbury.ac.nz>
2024-09-19 15:58:08 +1000

    Bug fix naiveEstimation.m
    Fixing another SD - Var mix up
    And trying with fixed known pop level SD parameters

--- toy_model/naiveEstimation.m
+++ toy_model/naiveEstimation.m
@@ -9,17 +9,17 @@ rng(5);
 
 prior_mean_a_min = -0.5; % Upper bound on prior for mean(a).
 prior_mean_a_max = 0.5;  % Lower bound on prior for mean(a).
-prior_sd_a_min = 0;     % Upper bound on prior for std(a).
-prior_sd_a_max = 0.5;     % Lower bound on prior for std(a).
+prior_sd_a_min = 0.3;     % Upper bound on prior for std(a).
+prior_sd_a_max = 0.3;     % Lower bound on prior for std(a).
 prior_mean_b_min = 0;   % Upper bound on prior for mean(b).
 prior_mean_b_max = 0.5;   % Lower bound on prior for mean(b).
-prior_sd_b_min = 0;     % Upper bound on prior for std(b).
-prior_sd_b_max = 0.5;     % Lower bound on prior for std(b).
+prior_sd_b_min = 0.3;     % Upper bound on prior for std(b).
+prior_sd_b_max = 0.3;     % Lower bound on prior for std(b).
 prior_cov_min = 0;      % Upper bound on prior for cov(a,b).
 prior_cov_max = 0;      % Lower bound on prior for cov(a,b).
 
 % Define sampling parameters
-nTrials = 500;         % Number of trials
+nTrials = 1000;         % Number of trials
 bestFraction = 0.8;     % Fraction of trials to accept
 bestNumber = ceil(nTrials*bestFraction); % Number of accepted trials.
 samplePopDist = @sampleNormal_Gamma; % Specify the pop-level distribution
@@ -31,13 +31,13 @@ par.x0 = 1;             % Initial conditions.
 par.tEnd = 1;         % Final time of model.
 par.dt = 0.2;    % Time step in model.
 nTime = par.tEnd/par.dt + 1;       % Number of time points.
-par.sNoise = 0.01;      % Standard deviation of observation noise.
+par.sNoise = 0.001;      % Standard deviation of observation noise.
 t = linspace(0,par.tEnd,nTime); % Vector of time points
 
 true_mean_a = 0;        % True mean(a) value.
 true_sd_a = 0.3;        % True std(a) value.
-true_mean_b = 0.2;      % True mean(b) value.
-true_sd_b = 0.2;          % True std(b) value.
+true_mean_b = 0.3;      % True mean(b) value.
+true_sd_b = 0.3;          % True std(b) value.
 true_cov = 0;           % True cov(a,b) value.
 
 true_means = [true_mean_a; true_mean_b];                        % Vector of true means.
@@ -65,7 +65,7 @@ loglikelihood = zeros(nTrials,1);
 % Perform trials and calculate log likelihood for each sample from the
 % priors.
 parfor iTrial = 1:nTrials
-    Theta = [mean_a_samples(iTrial);mean_b_samples(iTrial);sd_a_samples(iTrial);sd_b_samples(iTrial);cov_samples(iTrial)];
+    Theta = [mean_a_samples(iTrial);mean_b_samples(iTrial);sd_a_samples(iTrial)^2;sd_b_samples(iTrial)^2;cov_samples(iTrial)];
     loglikelihood(iTrial) = calcLogLik(Theta, data, samplePopDist, par);
 end
 
